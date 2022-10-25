ebc6ee0c9ccdec4426e77940cd2fc4b891ec1a63
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-25 12:42:50 +0200

    Initial commit

---
+++ TE20A_RaylibDemo/Avatar.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Avatar
+{
+  // Position
+  // Bild
+  // Movement
+
+  private Rectangle rect;
+  private Texture2D sprite;
+
+  public Avatar()
+  {
+    
+  }
+}
---
+++ TE20A_RaylibDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿global using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Världens bästa spel");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+  // LOGIK
+  // GRAFIK
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(Color.WHITE);
+
+  Raylib.EndDrawing();
+}
